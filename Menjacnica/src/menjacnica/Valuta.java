328aaed32e011e35bbc09b4d9c5ac4aa8f4af86b
ilijapavlovic95 <ilija95pav@gmail.com>
2016-03-22 09:12:30 +0100

    Generisane metode toString, equals i hashCode.

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -26,5 +26,49 @@ public class Valuta {
 	public void setKursevi(LinkedList<Kurs> kursevi) {
 		this.kursevi = kursevi;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursevi == null) ? 0 : kursevi.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((sifra == null) ? 0 : sifra.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kursevi == null) {
+			if (other.kursevi != null)
+				return false;
+		} else if (!kursevi.equals(other.kursevi))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (sifra == null) {
+			if (other.sifra != null)
+				return false;
+		} else if (!sifra.equals(other.sifra))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Valuta - naziv:" + naziv + ", sifra:" + sifra + ", kursevi:" + kursevi;
+	}
+	
+	
 
 }
